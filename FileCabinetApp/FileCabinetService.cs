e2cd15b4a80e094763cd6c231df5392afb150196
denios.vasilios@gmail.com <denios.vasilios@gmail.com>
2021-11-27 12:15:07 +0300

    Add implementation for stat command.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -24,7 +24,6 @@ public class FileCabinetService
 
     public int GetStat()
     {
-        // TODO: добавьте реализацию метода
-        return 0;
+        return this.list.Count;
     }
 }
--- FileCabinetApp/Program.cs
+++ FileCabinetApp/Program.cs
@@ -11,18 +11,22 @@ public static class Program
     private const int DescriptionHelpIndex = 1;
     private const int ExplanationHelpIndex = 2;
 
+    private static readonly FileCabinetService fileCabinetService = new FileCabinetService();
+
     private static bool isRunning = true;
 
     private static Tuple<string, Action<string>>[] commands = new Tuple<string, Action<string>>[]
     {
             new Tuple<string, Action<string>>("help", PrintHelp),
             new Tuple<string, Action<string>>("exit", Exit),
+            new Tuple<string, Action<string>>("stat", Stat),
     };
 
     private static string[][] helpMessages = new string[][]
     {
             new string[] { "help", "prints the help screen", "The 'help' command prints the help screen." },
             new string[] { "exit", "exits the application", "The 'exit' command exits the application." },
+            new string[] { "stat", "shows stat", "The 'stat' command shows stat." },
     };
 
     public static void Main(string[] args)
@@ -90,6 +94,12 @@ public static class Program
         }
 
         Console.WriteLine();
+    }
+
+    private static void Stat(string parameters)
+    {
+        var recordsCount = Program.fileCabinetService.GetStat();
+        Console.WriteLine($"{recordsCount} record(s).");
     }
 
     private static void Exit(string parameters)
