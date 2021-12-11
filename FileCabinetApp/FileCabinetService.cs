842b8f3e21cdfff421c16ff628d2cebacbbe398b
denios.vasilios@gmail.com <denios.vasilios@gmail.com>
2021-12-11 15:27:41 +0300

    Add implementation for edit command.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -12,11 +12,11 @@ public class FileCabinetService
 
     public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, short workExperience, decimal balance, char favLetter)
     {
-        this.CheckNames(firstName, lastName);
-        this.CheckDateOfBirth(dateOfBirth);
-        this.CheckWorkExperience(workExperience, dateOfBirth);
-        this.CheckBalance(balance);
-        this.CheckFavLetter(favLetter);
+        CheckNames(firstName, lastName);
+        CheckDateOfBirth(dateOfBirth);
+        CheckWorkExperience(workExperience, dateOfBirth);
+        CheckBalance(balance);
+        CheckFavLetter(favLetter);
 
         var record = new FileCabinetRecord
         {
@@ -34,11 +34,33 @@ public class FileCabinetService
         return record.Id;
     }
 
+    public void EditRecord(int id, string firstName, string lastName, DateTime dateOfBirth, short workExperience, decimal balance, char favLetter, int index)
+    {
+        CheckNames(firstName, lastName);
+        CheckDateOfBirth(dateOfBirth);
+        CheckWorkExperience(workExperience, dateOfBirth);
+        CheckBalance(balance);
+        CheckFavLetter(favLetter);
+
+        this.list[index] = new FileCabinetRecord
+        {
+            Id = id,
+            FirstName = firstName,
+            LastName = lastName,
+            DateOfBirth = dateOfBirth,
+            WorkExperience = workExperience,
+            Balance = balance,
+            FavChar = favLetter,
+        };
+    }
+
+    public int FindRecordIndexById(int id) => this.list.FindIndex(e => e.Id == id);
+
     public FileCabinetRecord[] GetRecords() => this.list.ToArray();
 
     public int GetStat() => this.list.Count;
 
-    private void CheckNames(string firstName, string lastName)
+    private static void CheckNames(string firstName, string lastName)
     {
         if (firstName is null)
         {
@@ -60,12 +82,12 @@ public class FileCabinetService
         {
             if (a.Length < 2)
             {
-                throw new ArgumentException($"{nameOfString} is too short");
+                throw new ArgumentException($"{nameOfString} is too short.");
             }
 
             if (a.Length > 60)
             {
-                throw new ArgumentException($"{nameOfString} is too long");
+                throw new ArgumentException($"{nameOfString} is too long.");
             }
         }
 
@@ -73,41 +95,41 @@ public class FileCabinetService
         {
             if (a.Trim().Length == 0)
             {
-                throw new ArgumentException($"{nameOfString} can't consist of only whitespaces");
+                throw new ArgumentException($"{nameOfString} can't consist of only whitespaces.");
             }
         }
     }
 
-    private void CheckDateOfBirth(DateTime date)
+    private static void CheckDateOfBirth(DateTime date)
     {
         if (date < new DateTime(1950, 1, 1) || date > DateTime.Now)
         {
-            throw new ArgumentException("Incorrect date");
+            throw new ArgumentException("Incorrect date.");
         }
     }
 
-    private void CheckWorkExperience(short workExperience, DateTime dateOfBirth)
+    private static void CheckWorkExperience(short workExperience, DateTime dateOfBirth)
     {
         int age = DateTime.Now.Subtract(dateOfBirth).Days / 365;
         if (workExperience < 0 || workExperience > age - 16)
         {
-            throw new ArgumentException("Incorrect work experisnce");
+            throw new ArgumentException("Incorrect work experisnce.");
         }
     }
 
-    private void CheckBalance(decimal balance)
+    private static void CheckBalance(decimal balance)
     {
         if (balance < 0)
         {
-            throw new ArgumentException("Incorrect balance");
+            throw new ArgumentException("Incorrect balance.");
         }
     }
 
-    private void CheckFavLetter(char favLetter)
+    private static void CheckFavLetter(char favLetter)
     {
         if (!char.IsLetter(favLetter))
         {
-            throw new ArgumentException("The char is not a letter");
+            throw new ArgumentException("The char is not a letter.");
         }
     }
 }
--- FileCabinetApp/Program.cs
+++ FileCabinetApp/Program.cs
@@ -2,7 +2,7 @@
 
 #pragma warning disable CS8604
 #pragma warning disable CS8602
-#pragma warning disable CS8600
+#pragma warning disable CS8601
 
 namespace FileCabinetApp;
 public static class Program
@@ -20,16 +20,18 @@ public static class Program
     private static Tuple<string, Action<string>>[] commands = new Tuple<string, Action<string>>[]
     {
             new Tuple<string, Action<string>>("help", PrintHelp),
-            new Tuple<string, Action<string>>("exit", Exit),
-            new Tuple<string, Action<string>>("stat", Stat),
             new Tuple<string, Action<string>>("create", Create),
+            new Tuple<string, Action<string>>("edit", Edit),
             new Tuple<string, Action<string>>("list", List),
+            new Tuple<string, Action<string>>("stat", Stat),
+            new Tuple<string, Action<string>>("exit", Exit),
     };
 
     private static string[][] helpMessages = new string[][]
     {
             new string[] { "help", "prints the help screen", "The 'help' command prints the help screen." },
             new string[] { "create", "creates new record", "The 'create' command creates new record." },
+            new string[] { "edit", "edits the record", "The 'edit' command edits the record." },
             new string[] { "list", "shows the list of records", "The 'list' command shows the list of records." },
             new string[] { "stat", "shows stat", "The 'stat' command shows stat." },
             new string[] { "exit", "exits the application", "The 'exit' command exits the application." },
@@ -108,44 +110,96 @@ public static class Program
         Console.WriteLine($"{recordsCount} record(s).");
     }
 
-    private static void Create(string parameters)
+    private static void Edit(string parameters)
     {
+        string firstName;
+        string lastName;
+        DateTime dateOfBirth;
+        short workExperience;
+        decimal balance;
+        char favLetter;
+
         while (true)
         {
-            Console.Write("First Name: ");
-            string firstName = Console.ReadLine();
+            int id = int.Parse(parameters);
+ 
+            int index = Program.fileCabinetService.FindRecordIndexById(id);
 
-            Console.Write("Last Name: ");
-            string lastName = Console.ReadLine();
+            if (index == -1)
+            {
+                Console.WriteLine($"#{id} record is not found.");
+                break;
+            }
 
-            Console.Write("Date of birth: ");
+            ReadDataForRecord(out firstName, out lastName, out dateOfBirth, out workExperience, out balance, out favLetter);
 
-            DateTime dateOfBirth = DateTime.Parse(
-                Console.ReadLine(),
-                CultureInfo.CreateSpecificCulture("en-US"),
-                DateTimeStyles.None);
-
-            Console.Write("Work experience: ");
-            short workExperience = short.Parse(Console.ReadLine());
-
-            Console.Write("Balance: ");
-            decimal balance = decimal.Parse(Console.ReadLine(), NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture);
-
-            Console.Write("Favorive letter: ");
-            char favLetter = char.Parse(Console.ReadLine());
-
-            int id;
             try
             {
-                id = Program.fileCabinetService.CreateRecord(firstName, lastName, dateOfBirth, workExperience, balance, favLetter);
+                Program.fileCabinetService.EditRecord(id, firstName, lastName, dateOfBirth, workExperience, balance, favLetter, index);
                 Console.WriteLine($"Record #{id} is created.");
                 return;
             }
             catch (Exception e)
             {
-                Console.WriteLine($"{e.Message}. Please, enter correct data:");
+                Console.WriteLine($"{e.Message} Please, enter the correct data:");
             }
         }
+    }
+
+    private static void Create(string parameters)
+    {
+        string firstName;
+        string lastName;
+        DateTime dateOfBirth;
+        short workExperience;
+        decimal balance;
+        char favLetter;
+
+        while (true)
+        {
+            ReadDataForRecord(out firstName, out lastName, out dateOfBirth, out workExperience, out balance, out favLetter);
+
+            try
+            {
+                int id = Program.fileCabinetService.CreateRecord(firstName, lastName, dateOfBirth, workExperience, balance, favLetter);
+                Console.WriteLine($"Record #{id} is created.");
+                return;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.Message} Please, enter the correct data:");
+            }
+        }
+    }
+
+    private static void ReadDataForRecord(
+        out string firstName,
+        out string lastName,
+        out DateTime dateOfBirth,
+        out short workExperience,
+        out decimal balance,
+        out char favLetter)
+    {
+        Console.Write("First Name: ");
+        firstName = Console.ReadLine();
+
+        Console.Write("Last Name: ");
+        lastName = Console.ReadLine();
+
+        Console.Write("Date of birth: ");
+        dateOfBirth = DateTime.Parse(
+            Console.ReadLine(),
+            CultureInfo.CreateSpecificCulture("en-US"),
+            DateTimeStyles.None);
+
+        Console.Write("Work experience: ");
+        workExperience = short.Parse(Console.ReadLine());
+
+        Console.Write("Balance: ");
+        balance = decimal.Parse(Console.ReadLine(), NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture);
+
+        Console.Write("Favorive letter: ");
+        favLetter = char.Parse(Console.ReadLine());
     }
 
     private static void List(string parameters)
