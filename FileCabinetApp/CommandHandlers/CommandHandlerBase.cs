7b3306fd222b8442aace6a2e59994d5be8692899
denios.vasilios@gmail.com <denios.vasilios@gmail.com>
2022-01-07 13:26:18 +0300

    Apply chain of responsibility parretn. Move all command handlers from Program to new CommandHadler class.

---
+++ FileCabinetApp/CommandHandlers/CommandHandlerBase.cs
@@ -0,0 +1,14 @@
+﻿namespace FileCabinetApp.CommandHandlers
+{
+    public abstract class CommandHandlerBase : ICommandHandler
+    {
+        private ICommandHandler nextHandler;
+
+        public abstract void Handle(AppCommandRequest appCommandRequest);
+
+        public void SetNext(ICommandHandler commandHandler)
+        {
+            this.nextHandler = commandHandler;
+        }
+    }
+}
